1d9d1e1dbe1f06d7e7fb2ddd7acba0252fb302d6
Luke Abel <luke.abel@gmail.com>
2017-07-25 00:43:34 -0400

    exercise 1-13

--- 1-5/1-12.c
+++ 1-5/1-12.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+#define IN  1
+#define OUT 0
+
+// print one word per line
 main()
 {
+  int c, state;
 
+  state = OUT;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\n' || c == ' ' || c == '\t') {
+      if (state == IN) {
+        putchar('\n');
+      }
+      state = OUT;
+    } else if (state == OUT) {
+      state = IN;
+      putchar(c);
+    } else {
+      putchar(c);
+    }
+  }
 }
