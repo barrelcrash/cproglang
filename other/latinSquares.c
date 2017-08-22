78683576d075caf37f085723cdc19f889cddcfe9
Luke <luke.abel@gmail.com>
2017-08-22 12:37:28 -0400

    Update latinSquares.c

--- other/latinSquares.c
+++ other/latinSquares.c
@@ -12,9 +12,6 @@ int main() {
 
   int side1 = 5;
   int square1[25] = {1, 2, 3, 4, 5, 5, 1, 2, 3, 4, 4, 5, 1, 2, 3, 3, 4, 5, 1, 2, 2, 3, 4, 5, 1};
-  int side2 = 4;
-  int square2[16] = {1, 2, 3, 4, 1, 3, 2, 4, 2, 3, 4, 1, 4, 3, 2, 1};
-
   printf("square1 is latin?: %d\n", detectLatin(side1, square1));
 
   if (detectLatin(side1, square1)) {
@@ -25,9 +22,24 @@ int main() {
 
   printf("\n");
 
+  int side2 = 4;
+  int square2[16] = {1, 2, 3, 4, 1, 3, 2, 4, 2, 3, 4, 1, 4, 3, 2, 1};
+
   printf("square2 is latin?: %d\n", detectLatin(side2, square2));
   if (detectLatin(side2, square2)) {
     reduceAndPrint(side2, square2);
+  } else {
+    printf("Not a latin square and not reduceable!\n");    
+  }
+
+  printf("\n");
+
+  int side3 = 5;
+  int square3[25] = {1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5};
+
+  printf("square3 is latin?: %d\n", detectLatin(side3, square3));
+  if (detectLatin(side3, square3)) {
+    reduceAndPrint(side3, square3);
   } else {
     printf("Not a latin square and not reduceable!\n");    
   }
@@ -39,11 +51,21 @@ int detectLatin(int side, int sq[]) {
   int isLatin = TRUE;
 
   for (int i = 0; i < (side * side); i++) {
-    for (int j = side; j < (side * side); j += side) {
+    // check column
+    for (int j = side; j <= (side * side); j += side) {
       if (i >= side && i - j >= 0) {
         if (sq[i] == sq[i - j]) {
           isLatin = FALSE;
         }
+      }
+    }
+
+    // check row
+    int rowMod = i % 5;
+    int rowStart = i - rowMod;
+    for (int k = rowStart; k < (rowStart + 5); k++) {
+      if (i != k && sq[i] == sq[k]) {
+        isLatin = FALSE;
       }
     }
   }
@@ -53,19 +75,21 @@ int detectLatin(int side, int sq[]) {
 
 void reduceAndPrint(int side, int sq[]) {
   int array[side][side];
+  int array2[side][side];
   int j;
 
   // create 2d array
   for (int i = 0; i < side; i++) {
     for (int j = 0; j < side; j++) {
-      array[j][i] = sq[(i * side) + j];
+      int index = (i * side) + j;
+      array[i][j] = sq[index];
     }
   }
-
   
   //  bubble sort
-  int swapped = FALSE;
+  int swapped;
   do {
+    swapped = FALSE;
     for (int i = 1; i < side; i++) {
       if (array[i - 1][0] > array[i][0]) {
         swapRow(side, array[i - 1], array[i]);
@@ -92,3 +116,4 @@ void swapRow(int side, int r[], int o[]) {
     o[i] = temp;
   }
 }
+
