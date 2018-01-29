7e84da25bf2d188ca94fefd268770c416bdff60f
Luke <luke.abel@gmail.com>
2018-01-29 15:20:41 -0500

    Create 5-03.c

---
+++ 5/5-03.c
@@ -0,0 +1,48 @@
+/* Write a pointer version of the function strcat
+ * that we showed in Chapter 2: strcat(s,t) copies
+ * the string t to the end of s.
+ */
+
+#include <stdio.h>
+
+void astrcat(char[], char[]);
+void pstrcat(char*, char*);
+
+int main()
+{
+
+  char string1[] = "all";
+  char string2[] = "your";
+  char string3[] = "base";
+  char string4[] = "are";
+
+  astrcat(string1, string2);
+  printf(string1);
+  
+  printf("\n");
+
+  pstrcat(string3, string4);
+  printf(string3);
+}
+
+// strcat - array version
+void astrcat(char s[], char t[])
+{
+  int i, j;
+
+  i = j = 0;
+  while (s[i] != '\0') // find end of s
+    i++;
+  while ((s[i++] = t[j++]) != '\0') // copy t
+    ;
+}
+
+// strcat - pointer version
+void pstrcat(char *s, char *t)
+{
+  while (*s != '\0') // find end of s
+    *s++; 
+
+  while ((*s++ = *t++) != '\0') // copy t
+    ;
+}
