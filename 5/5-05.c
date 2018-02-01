396f794faa3bf93536434773bc725e3801cb3877
Luke Abel <luke.abel@gmail.com>
2018-01-31 21:48:20 -0500

    exercise 5.5

--- 5/5-05.c
+++ 5/5-05.c
@@ -10,7 +10,8 @@
 void astrcpy(char*, char*);
 void astrncpy(char*, char*, int);
 
-void nstrcat(char*, char*, int);
+void astrcat(char*, char*);
+void astrncat(char*, char*, int);
 
 int astrcmp(char*, char*);
 int astrncmp(char*, char*, int);
@@ -19,16 +20,26 @@ int main() {
 
   //astrncpy
   char cpy1[] = "hello\n";
-  char cpy2[] = "zzzzz\n";
+  char cpy2[] = "uv1234\n";
   astrncpy(cpy1, cpy2, 3);
-  printf(cpy1); // should be "zzzlo"
+  printf(cpy1); // should be "uv1lo"
 
+  putchar('\n');
+
+  // astrncat
+  char cat1[] = "goodbye";
+  char cat2[] = "hijklmn";
+  astrncat(cat1, cat2, 3);
+  printf(cat1); // should be "goodbyehij"
+
+  putchar('\n');
 
   // astrncmp
   char cmp1[] = "abcdefg";
   char cmp2[] = "abcdxyz";
   printf("%d\n", astrcmp(cmp1, cmp2)); // should not be 0
   printf("%d\n", astrncmp(cmp1, cmp2, 4)); // should be 0
+  printf("%d\n", astrncmp(cmp1, cmp2, 5)); // should not be 0
 }
 
 void astrcpy(char *s, char *t)
@@ -39,13 +50,28 @@ void astrcpy(char *s, char *t)
 
 void astrncpy(char *s, char *t, int n)
 {
-  int nt = *t;
-  while ((*s++ = *t++) && t - nt <= n)
+  char *nt = t;
+  while ((*s++ = *t++) && t - nt < n)
     ;
 }
 
-void nstrcat(char *s, char *t, int n)
+void astrcat(char *s, char *t)
 {
+  while ((*s))
+    *s++;
+  while ((*s++ = *t++))
+    ;
+}
+
+void astrncat(char *s, char *t, int n)
+{
+  while ((*s))
+    *s++;
+
+  char *nt = t;
+  while ((*s++ = *t++) && t - nt < n)
+    ;
+  *s = '\0';
 }
 
 int astrcmp(char *s, char *t)
@@ -58,9 +84,11 @@ int astrcmp(char *s, char *t)
 
 int astrncmp(char *s, char *t, int n)
 {
-  int nt = *t;
-  for ( ; *s == *t && t - nt <= n; s++, t++)
-    if (*s == '\0')
+  char *nt = t;
+  for ( ; *s == *t && t - nt < n; s++, t++) {
+    if ((*s == '\0') || (t - nt + 1 == n)) {
       return 0;
+    }
+  }
   return *s - *t;
 }
