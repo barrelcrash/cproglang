f2dcbf9c868ac49e9a1eedc021f140ed076ae285
Luke <luke.abel@gmail.com>
2018-02-28 16:50:26 -0500

    finishing up raw mode

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,35 +1,60 @@
+/** includes **/
+
 #include <ctype.h>
+#include <errno.h>
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+/** data **/
+
 struct termios orig_termios;
 
+/** terminal **/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // reset term attr
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) // reset term attr
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios); // move terminal attr to struct
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr"); // move terminal attr to struct
 
   atexit(disableRawMode); // invoke this func at exit
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON); // bit-flipping, turn off echo & canonical
+  // bit flipping
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON); // disable Ctrl-S, Ctrl-Q, Ctrl-M processing, misc
+  raw.c_oflag &= ~(OPOST); // disable output post-processing
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); // disable echo, canonical, Ctrl-Z, Ctrl-C
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // set terminal attr
 }
+
+/** init **/
 
 int main() {
   enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno!= EAGAIN) die("read");
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n", c, c);
+      printf("%d ('%c')\r\n", c, c);
     }
+    if (c == 'q')
+      break;
   }
   return 0;
 }
