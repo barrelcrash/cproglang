4887ab1be4e8bae7a591a3a5de98b95083d47cbc
Luke <luke.abel@gmail.com>
2018-02-28 09:33:16 -0500

    canonical mode, reading keypresses

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,18 +1,35 @@
-#include <termio.h>
+#include <ctype.h>
+#include <termios.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // reset term attr
+}
 
 void enableRawMode() {
-  struct termios raw;
+  tcgetattr(STDIN_FILENO, &orig_termios); // move terminal attr to struct
 
-  tcgetattr(STDIN_FILENO, &raw);
+  atexit(disableRawMode); // invoke this func at exit
 
-  raw.c_lflag &= ~(ECHO);
-
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON); // bit-flipping, turn off echo & canonical
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // set terminal attr
 }
 
 int main() {
+  enableRawMode();
+
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
