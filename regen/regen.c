89b7efa8161c4039a90b3ac65f47fd84aaa73e63
Luke <luke.abel@gmail.com>
2018-04-04 16:53:15 -0400

    regency - literal char rule handling

--- regen/regen.c
+++ regen/regen.c
@@ -6,15 +6,21 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 
 #define MAX 5000
 #define MAX_RULES 100
 
-#define RULE_INIT {NULL, 0, NULL, 0}
+#define RULE_INIT {0, NULL, 0}
+
+enum ruletypes {
+  INVALID = 0,
+  LITERAL
+};
 
 struct rule {
-  char *key;
-  int keylen;
+  int type;
   char *value;
   int valuelen;
 };
@@ -24,7 +30,8 @@ struct rule rules[MAX_RULES];
 int ruleslen = 0;
 
 /** function declarations **/
-void parseRuleString(char**);
+void parseRuleString(struct rule*, char**);
+struct rule *createRule(char[]);
 
 int main(int argc, char **argv) {
 
@@ -44,17 +51,34 @@ int main(int argc, char **argv) {
     }
   }
 
-  parseRuleString(argv); 
-}
+  // initialize ruleset
+  struct rule rules[MAX_RULES];
+  parseRuleString(&rules[0], argv); 
 
-// TODO: create dynamic rule building
-
-void parseRuleString(char **s) {
-  printf("rule string: %s\n", *s);
-  int rulelen = sizeof(*s) / sizeof(**s);
-
-  // TODO: implement literal rule parsing
-  while (--rulelen > 0) {
+  // TODO: build string from literal rules
+  for (int i = 0; rules[i].type > 0; i++) {
+    printf("rule type: %d\n", rules[i].type);
+    printf("rule string: %s\n", rules[i].value);
   }
 }
 
+void parseRuleString(struct rule *rulep, char **s) {
+  printf("rule string: %s\n", *s);
+
+  int slen = strlen(*s);
+
+  // TODO: implement digit rule parsing
+  while (--slen >= 0) {
+    char buf[] = {*(*s)++, '\0'};
+    *rulep = *createRule(buf);
+    rulep++;
+  }
+}
+
+struct rule *createRule(char token[]) {
+  struct rule *temp = malloc(sizeof(struct rule));
+  temp->type = LITERAL;
+  temp->value = strdup(token);
+  temp->valuelen = strlen(token);
+  return temp;
+}
