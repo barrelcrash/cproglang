4cb8bdd99f136673e08237a46a08b684571b497a
Luke <luke.abel@gmail.com>
2018-04-06 12:17:32 -0400

    result string building from rules

--- regen/regen.c
+++ regen/regen.c
@@ -32,6 +32,7 @@ int ruleslen = 0;
 /** function declarations **/
 void parseRuleString(struct rule*, char**);
 struct rule *createRule(char[]);
+char *concatRuleValues(struct rule*);
 
 int main(int argc, char **argv) {
 
@@ -53,17 +54,23 @@ int main(int argc, char **argv) {
 
   // initialize ruleset
   struct rule rules[MAX_RULES];
+
   parseRuleString(&rules[0], argv); 
 
-  // TODO: build string from literal rules
-  for (int i = 0; rules[i].type > 0; i++) {
-    printf("rule type: %d\n", rules[i].type);
-    printf("rule string: %s\n", rules[i].value);
+  printf("regency result: %s\n", concatRuleValues(rules));
+}
+
+char *concatRuleValues(struct rule *rulep) {
+  char *temp = malloc(sizeof(char));
+  while (rulep->type > 0) {
+    realloc(temp, sizeof(temp) + sizeof(rulep->value));
+    strcat(temp, rulep->value);
+    rulep++;
   }
+  return temp;
 }
 
 void parseRuleString(struct rule *rulep, char **s) {
-  printf("rule string: %s\n", *s);
 
   int slen = strlen(*s);
 
