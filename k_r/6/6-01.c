4c5d3f10bcd5b376657b0757450a2c8c1f8525d0
Luke Abel <luke.abel@gmail.com>
2018-05-09 22:11:27 -0400

    add .swo to gitignore, 6-1 prep

---
+++ k_r/6/6-01.c
@@ -0,0 +1,61 @@
+/* 6-1: our version of getword does not properly handle
+ * underscores, string constants, comments, or preprocessor
+ * control lines. Write a better version.
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+
+#define BUFSIZE 100
+#define MAXWORD 100
+
+int getword(char *, int);
+
+int main() {
+  int n;
+  char word[MAXWORD];
+
+  while(getword(word, MAXWORD) !=EOF)
+    printf("%s\n", word);
+}
+
+int getword(char *word, int lim) {
+  int c, getch(void);
+  void ungetch(int);
+  char *w = word;
+
+  while (isspace(c = getch()))
+    ;
+  if (c != EOF)
+    *w++ = c;
+  if (!isalpha(c)) {
+    *w = '\0';
+    return c;
+  }
+  for ( ; --lim > 0; w++)
+    if (!isalnum(*w = getch())) {
+      ungetch(*w);
+      break;
+    }
+  *w = '\0';
+  return word[0];
+}
+
+/*
+ * CHAR BUFFER
+ */
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buf */
+
+int getch(void) /* get a (possibly pushed back) character */
+{
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* push character back on input */
+{
+  if(bufp >= BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
+}
