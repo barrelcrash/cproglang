89cfe23c8b846100ffed6b71ce282a034984e2c8
Luke Abel <luke.abel@gmail.com>
2018-05-16 20:51:11 -0400

    6-2: setup

--- k_r/6/6-01.c
+++ k_r/6/6-01.c
@@ -19,7 +19,15 @@ struct key {
 } keytab[] = {
   "auto", 0,
   "break", 0,
-  "case", 0
+  "case", 0,
+  "char", 0,
+  "const", 0,
+  "continue", 0,
+  "default", 0,
+  "unsigned", 0,
+  "void", 0,
+  "volatile", 0,
+  "while", 0,
 };
 
 int main() {
@@ -37,7 +45,24 @@ int main() {
   return 0;
 }
 
-// TODO: copy over binsearch
+/* binsearch: find word in tab[0]...tab[n-1] */
+int binsearch(char *word, struct key tab[], int n) {
+  int cond;
+  int low, high, mid;
+
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if ((cond = strcmp(word, tab[mid].word)) < 0)
+      high = mid - 1;
+    else if (cond > 0)
+      low = mid + 1;
+    else
+      return mid;
+  }
+  return -1;
+}
 
 int getword(char *word, int lim) {
   int c, getch(void);
